57e3be2a4f6d772cd920ce9e0a0c8c905e8234de
LeozeraVal <leovmorales@hotmail.com>
2020-12-14 19:28:03 -0300

    Comentario Gerenciador.

--- src/xadrez/Gerenciador.java
+++ src/xadrez/Gerenciador.java
@@ -3,7 +3,7 @@ package xadrez;
 public class Gerenciador {
 
     public static void main(String[] args) throws Exception {
-        Jogo xadrez = new Jogo();
+        //Jogo xadrez = new Jogo();
     }
     
 }
